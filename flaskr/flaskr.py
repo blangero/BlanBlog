1b231f9b300f555182f5528f7564906bdb869d3b
Blangero <blangero1987@gmail.com>
2017-08-15 23:28:26 +0800

    try adding markdown editor

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -10,8 +10,8 @@ app.config.from_object(__name__) # load config from this file , flaskr.py
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'flaskr.db'),
     SECRET_KEY='development key',
-    USERNAME='admin',
-    PASSWORD='default'
+    USERNAME='blangero',
+    PASSWORD='wangxinghua'
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
@@ -20,6 +20,7 @@ def connect_db():
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
+
 def get_db():
     """Opens a new database connection if there is none yet for the
     current application context.
@@ -27,11 +28,13 @@ def get_db():
     if not hasattr(g, 'sqlite_db'):
         g.sqlite_db = connect_db()
     return g.sqlite_db
+
 @app.teardown_appcontext
 def close_db(error):
     """Closes the database again at the end of the request."""
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
+
 def init_db():
     db = get_db()
     with app.open_resource('schema.sql', mode='r') as f:
@@ -43,6 +46,7 @@ def initdb_command():
     """Initializes the database."""
     init_db()
     print('Initialized the database.')
+
 @app.route('/')
 def show_entries():
     db = get_db()
@@ -50,6 +54,7 @@ def show_entries():
     entries = cur.fetchall()
     return render_template('markdown_editor.html', entries=entries)
 #    return render_template('show_entries.html', entries=entries)
+
 @app.route('/add', methods=['POST'])
 def add_entry():
     if not session.get('logged_in'):
@@ -60,6 +65,7 @@ def add_entry():
     db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
